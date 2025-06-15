0dfcad4eebdc8576f8180e8a682bcd8db7c194c6
David Just <daviddjustt@gmail.com>
2025-06-15 13:33:11 -0300

    Inicia projeto !

---
+++ solarenergy/solarenergy/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from solarenergy.users.models import User
+from solarenergy.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
