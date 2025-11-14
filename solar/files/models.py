c181042349f178d54066a7707a1a7528654fe783
david <daviddjustt@gmail.com>
2025-11-14 09:09:50 -0300

    Modelo de lista de materiais adicionado

--- solar/files/models.py
+++ solar/files/models.py
@@ -4,10 +4,13 @@ from django.utils import timezone
 from solar.documents.utils import get_document_upload_path, validate_file_size
 from solar.documents.models import ClientProject
 from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
+import os
+import uuid
+from django.utils import timezone
+from django.utils.text import slugify
+
 from solar.files.utils import ( 
-    validate_file_extension, 
-    get_document_upload_path,
-    validate_file_size, 
     DOCUMENT_TYPE_CHOICES, 
     STATUS_CHOICES, 
     IN_ANALYSIS, 
