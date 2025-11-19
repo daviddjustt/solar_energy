2237d9aea2809b2e60acb884791b16d0ad62d88d
david just <daviddjustt@gmail.com>
2025-11-19 19:39:59 -0300

    Regra para o endpoint de unidades consumidoras

--- solar/documents/serializers.py
+++ solar/documents/serializers.py
@@ -73,7 +73,7 @@ class ConsumerUnitSerializer(serializers.ModelSerializer):
                 })
             
         elif prioridade_is_porcentagem == False:
-            if priority_level is None or (isinstance(priority_level, int) and not priority_level.strip()):
+            if priority_level is None or (isinstance(priority_level, int)):
                 raise serializers.ValidationError({
                     'porcentagem': 'O campo "priority_level" não pode ser vazio, nulo ou branco quando "prioridade_is_porcentagem" é True.'
                 })
