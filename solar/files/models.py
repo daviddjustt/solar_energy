57ece90f99c0a2d588f23542e6380edf9dfeebb8
david just <daviddjustt@gmail.com>
2025-11-26 13:43:28 -0300

    Campo de reject reason adicionado

--- solar/files/models.py
+++ solar/files/models.py
@@ -68,6 +68,7 @@ class Document(BaseModel, ArquivoMixin):
         default=IN_ANALYSIS, # Documentos recém-enviados começam "Em Análise"
         verbose_name="Status do Documento"
     )
+    rejection_reason = models.TextField(blank=True,null=True,verbose_name="Motivo da rejeição")
     # `uploaded_at` é fornecido por BaseModel.created_at
     approved_at = models.DateTimeField(blank=True, null=True, verbose_name="Data de Aprovação")
     
@@ -160,6 +161,7 @@ class DocumentUser(Document):
         on_delete=models.CASCADE,
         related_name='documents_user'
     )
+
 
     class Meta:
         abstract = False
