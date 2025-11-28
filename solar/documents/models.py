1e14040abc8de9d003002be258fd0b685bc79758
david just <daviddjustt@gmail.com>
2025-11-28 13:25:15 -0300

    Resolvendo erro do serializer de uploaddocuments

--- solar/documents/models.py
+++ solar/documents/models.py
@@ -514,11 +514,6 @@ class ProjectDocument(BaseModel, ArquivoMixin):
         """
         # Executar validações
         self.full_clean()
-
-        if self.status == ProjectDocument.APPROVED and not self.approved_at:
-            self.approved_at = timezone.now()
-        elif self.status != ProjectDocument.APPROVED and self.approved_at:
-            self.approved_at = None
         
         # Salvar o objeto
         super().save(*args, **kwargs)
