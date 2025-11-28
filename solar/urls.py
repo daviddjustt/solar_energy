561d445fdccc05e33722626de91febed96f9dbe1
david just <daviddjustt@gmail.com>
2025-11-28 13:18:32 -0300

    Resolvendo erro do serializer de uploaddocuments

--- solar/urls.py
+++ solar/urls.py
@@ -68,7 +68,7 @@ urlpatterns = [
 
     # Endpoints para Documentos (aninhados sob o projeto)
     path('api/v1/projects/<int:project_pk>/documents/', ProjectDocumentListView.as_view(), name='project-document-list-create'),
-    path('api/v1/projects/<int:project_pk>/documents/<int:pk>/', ProjectDocumentDetailView.as_view(), name='project-document-detail-update-delete'),
+    # path('api/v1/projects/<int:project_pk>/documents/<int:pk>/', ProjectDocumentDetailView.as_view(), name='project-document-detail-update-delete'),
     
     path('api/v1/projects/<int:project_pk>/consumer_units/', ConsumerUnitListView.as_view(), name='project-consumer-unit-list-create'),
     path('api/v1/projects/<int:project_pk>/consumer_units/<int:pk>/', ConsumerUnitDetailView.as_view(), name='project-consumer-unit-detail-update-delete'),
