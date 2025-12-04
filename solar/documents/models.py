903996bac4898d713d8f32bdaf50540736affd1a
aibta <daviddjustt@gmail.com>
2025-12-04 11:32:40 -0300

    Teste 1

--- solar/documents/models.py
+++ solar/documents/models.py
@@ -162,7 +162,7 @@ class ProjectDocument(Document): # Herda do modelo abstrato Document
     Representa um documento específico que pertence a um projeto.
     """
     project = models.ForeignKey(
-        'solar.projects.Projeto', # Referência ao seu modelo Projeto
+        'projects.Projeto', # Referência ao seu modelo Projeto
         on_delete=models.CASCADE,
         related_name='documents', # Nome para acessar documentos a partir de um projeto
         verbose_name="Projeto"
