6f0545673ae586b7a6688ec3f46c9adfceae687c
aibta <daviddjustt@gmail.com>
2025-12-04 11:22:31 -0300

    Teste 1

---
+++ solar/urls.py
@@ -0,0 +1,9 @@
+# solar/urls.py
+
+from django.urls import path
+
+urlpatterns = [
+    # Esta lista está vazia, o que significa que este arquivo de URLs
+    # não define nenhum padrão de URL diretamente.
+    # O Django precisa que a variável 'urlpatterns' exista e seja iterável.
+]
