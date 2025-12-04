2d2e96b1083d87f179eb1d37e35a9001d4561abe
aibta <daviddjustt@gmail.com>
2025-12-04 10:47:06 -0300

    Teste 1

--- solar/config/common.py
+++ solar/config/common.py
@@ -27,7 +27,7 @@ class Common(Configuration):
         'simple_history',  # ADICIONADO - estava faltando
         
         # Your apps
-        'solar.users',
+        'solar.users.apps.UsersConfig',
         'solar.documents',
         'solar.project',
     )
