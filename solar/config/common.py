6fb4d47601ee7df7ff10d7380e4e65006808ee36
aibta <daviddjustt@gmail.com>
2025-12-22 17:06:15 -0300

    Adicionando o sendgrid, teste 2

--- solar/config/common.py
+++ solar/config/common.py
@@ -79,7 +79,7 @@ class Common(Configuration):
     EMAIL_USE_TLS = strtobool(os.getenv('EMAIL_USE_TLS', 'no'))
     EMAIL_USE_SSL = strtobool(os.getenv('EMAIL_USE_SSL', 'no'))
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@sntechsolar.com')
-    ENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+    SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
     
     ADMINS = (
