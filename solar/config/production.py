e22ce62ec03638c176622894930b0b7a3a4ed0df
daviddjustt <daviddjustt@gmail.com>
2026-05-06 14:34:23 -0300

    Cloud adicionado

--- solar/config/production.py
+++ solar/config/production.py
@@ -3,62 +3,29 @@ from .common import Common
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 class Production(Common):
-    INSTALLED_APPS = Common.INSTALLED_APPS
+    # Herda os apps do common.py e adiciona o gunicorn
+    INSTALLED_APPS = Common.INSTALLED_APPS + ("gunicorn", )
+    
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-    # Site
-    # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = ["*"]
-    INSTALLED_APPS += ("gunicorn", )
 
     # ==========================================
     # FILE UPLOAD SETTINGS
     # ==========================================
-    if os.getenv('RAILWAY_ENVIRONMENT'):
-        MEDIA_ROOT = '/app/media'  # ← Volume do Railway
-    else:
-        MEDIA_ROOT = BASE_DIR / 'media'  # ← Desenvolvimento local
-    
-    MEDIA_URL = '/media/'
-
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static',
-    ]
-
     FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
     DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
     
-    # Permissões de arquivo
-    FILE_UPLOAD_PERMISSIONS = 0o644
-    FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o755
-    
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/2.0/howto/static-files/
-    # http://django-storages.readthedocs.org/en/latest/index.html
-    STORAGES = {
-        "default": {
-            "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-        },
-        "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-        },
-    }
-    INSTALLED_APPS += ('storages',)
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
-    MEDIA_URL = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
+    # As configurações de Cloudinary e Arquivos Estáticos (Static) 
+    # já estão sendo herdadas perfeitamente do common.py! 
+    # Não precisamos repeti-las aqui.
 
-    # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
-    # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
-    # 86400 = (60 seconds x 60 minutes x 24 hours)
-    AWS_HEADERS = {
-        'Cache-Control': 'max-age=86400, s-maxage=86400, must-revalidate',
-    }
+    # ==========================================
+    # SEGURANÇA EM PRODUÇÃO (RAILWAY)
+    # ==========================================
+    # Garante que o Django entenda o HTTPS do Railway e não dê loop infinito
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
