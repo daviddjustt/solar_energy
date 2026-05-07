27112aad876e68fc4167283105c7b70d6c4de08a
daviddjustt <daviddjustt@gmail.com>
2026-05-06 21:19:04 -0300

    Migração para novo campo chamado observações

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,6 @@ done
 # 4. SINCRONIZAÇÃO AUTOMÁTICA (Resolve o aviso do Log)
 echo "🔄 Gerando migrações automáticas para sincronizar models.py..."
 # Forçamos a criação para evitar o aviso "models have changes not reflected"
-python manage.py makemigrations documents --noinput
-python manage.py makemigrations users --noinput
-python manage.py makemigrations files --noinput
 
 echo "🔄 Aplicando migrações..."
 python manage.py migrate --noinput
--- solar/documents/migrations/0002_clientproject_observacoes.py
+++ solar/documents/migrations/0002_clientproject_observacoes.py
@@ -7,7 +7,7 @@ class Migration(migrations.Migration):
     dependencies = [
         # ATENÇÃO: Coloque aqui o nome exato do arquivo da migração que você me enviou 
         # (exemplo: '0001_initial' ou '0015_alguma_coisa'), sempre sem o ".py"
-        ('documents', '0002_clientproject_created_by_and_more'), 
+        ('documents', '0001_initial'), 
     ]
 
     operations = [
