cb6ec6ee46328ff9ab7bdd93d31d85575749c45a
unknown <daviddjustt@gmail.com>
2026-05-27 17:33:48 -0300

    Correção de erro de escrita

--- solar/documents/models.py
+++ solar/documents/models.py
@@ -18,7 +18,7 @@ class AndamentoDoProjeto(models.TextChoices):
     ANALISE_DE_DOCUMENTOS = 'Em análise de documentos'
     EXECUCAO = "Projeto em Execução"
     ANALISE_DE_PAGAMENTOS = "Analisando pagamento da TRT/ART"
-    PAGAMENTO_TRT_ART = 'Analisando pagamento da TRT/ART'
+    PAGAMENTO_TRT_ART = 'Pagamento da TRT/ART'
     PAGAMENTO_DO_PROJETO = 'Pagamento do projeto'
     ANALISE_TECNICA = 'Projeto em análise técnica'
     APROVADO = 'Projeto aprovado'
