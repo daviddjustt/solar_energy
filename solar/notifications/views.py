e29480f5dd442970c97021cf1fc285c04f5de970
unknown <daviddjustt@gmail.com>
2026-05-31 17:44:47 -0300

    Introdução do novo app de Notificações

--- solar/notifications/views.py
+++ solar/notifications/views.py
@@ -1,12 +1,12 @@
 # solar/notifications/views.py
+import time
 from django.utils import timezone
 from rest_framework import viewsets, status
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-import time
 from django.http import StreamingHttpResponse
-from rest_framework.decorators import api_view, permission_classes
+
 from .models import Notification
 from .serializers import NotificationSerializer  # Crie um ModelSerializer padrão para o modelo
 
