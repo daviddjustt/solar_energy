ec8d4fbd456e4620cc7783c6ec42d093c4511b88
Ellen <daviddjustt@gmail.com>
2026-06-01 21:19:22 -0300

    Novo campo para o projeto chamado pedido_vistoria criado

--- solar/documents/migrations/0003_clientproject_pedido_vistoria.py
+++ solar/documents/migrations/0003_clientproject_pedido_vistoria.py
@@ -6,7 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('documents', '0001_initial'), 
+        # Voltamos a depender do 0002 para manter a linha reta no app documents
+        ('documents', '0002_clientproject_observacoes'), 
+    ]
+
+    # ESSA É A MÁGICA: Avisa o Django para injetar este campo antes do app de notificações rodar
+    run_before = [
+        ('documents', '0001_initial'),
     ]
 
     operations = [
