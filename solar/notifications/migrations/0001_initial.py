c078acdac181d1f8f7c319285a6fdf35b88a5e6e
Ellen <daviddjustt@gmail.com>
2026-06-02 18:43:37 -0300

    Novo campo para o projeto chamado pedido_vistoria criado

--- solar/notifications/migrations/0001_initial.py
+++ solar/notifications/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     # antes da tabela de notificações ser criada.
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('documents', '__latest__'), # Pega a última migração existente do app documents
+        ('documents', '0001_initial'), 
     ]
 
     operations = [
