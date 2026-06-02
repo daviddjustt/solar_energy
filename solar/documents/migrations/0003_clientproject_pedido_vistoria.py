e0db81e2d400f0257490ca4d75b1497f079ffaaa
Ellen <daviddjustt@gmail.com>
2026-06-01 21:13:10 -0300

    Novo campo para o projeto chamado pedido_vistoria criado

--- solar/documents/migrations/0003_clientproject_pedido_vistoria.py
+++ solar/documents/migrations/0003_clientproject_pedido_vistoria.py
@@ -6,9 +6,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        # Substitua '0002_add_observacoes' pelo nome EXATO do arquivo anterior 
+        # Substitua '0002_clientproject_observacoes' pelo nome EXATO do arquivo anterior 
         # que adicionou o campo de observações, sem a extensão .py
-        ('documents', '0002_add_observacoes'), 
+        ('documents', '0002_clientproject_observacoes'), 
     ]
 
     operations = [
