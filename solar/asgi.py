f39f8d6f2bb9a7014c2bfa20adb453a417af8cf4
Ellen <daviddjustt@gmail.com>
2026-06-03 20:53:09 -0300

    refatoracao : infraestrutura e consiguracoes adicionadas

--- solar/asgi.py
+++ solar/asgi.py
@@ -1,21 +1,25 @@
 import os
+import configurations  # <--- IMPORTANTE: Importar aqui
+
+# 1. Configurar o ambiente antes de tudo
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solar.config.local') # Ajuste o caminho se necessário
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')               # O nome da sua classe no local.py
+configurations.setup()                                               # <--- A MÁGICA: Isso inicializa o django-configurations
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-# Importe o seu consumer (vamos criar no Passo 3)
 #from solar.notifications.consumers import NotificationConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solar.settings')
-
-# Inicializa o handler HTTP do Django
+# 2. Agora o Django pode carregar as configurações corretamente
 django_asgi_app = get_asgi_application()
 
 #application = ProtocolTypeRouter({
-#        "http": django_asgi_app,
-#        "websocket": AuthMiddlewareStack(
-#            URLRouter([
-#                path("ws/notifications/<int:user_id>/", NotificationConsumer.as_asgi()),
-#            ])
-#        ),
-#    })
+#    "http": django_asgi_app,
+#    "websocket": AuthMiddlewareStack(
+#        URLRouter([
+#            path("ws/notifications/<int:user_id>/", NotificationConsumer.as_asgi()),
+#        ])
+#    ),
+#})
