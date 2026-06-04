f24b0c72943674d14f346073e52ab7f322404d44
Ellen <daviddjustt@gmail.com>
2026-06-04 12:51:56 -0300

    Novo campo para o projeto chamado pedido_vistoria criado

--- solar/notifications/consumers.py
+++ solar/notifications/consumers.py
@@ -1,40 +1,11 @@
 import json
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        user = self.scope['user']
-        
-        if user.is_authenticated:
-            # Definimos o grupo pessoal para uso no disconnect
-            self.group_names = [f'user_{user.id}']
-            
-            # 1. Adiciona ao grupo pessoal
-            await self.channel_layer.group_add(self.group_names[0], self.channel_name)
-
-            # 2. Adiciona aos grupos do Django
-            user_groups = await database_sync_to_async(list)(user.groups.values_list('name', flat=True))
-            for group_name in user_groups:
-                self.group_names.append(group_name)
-                await self.channel_layer.group_add(group_name, self.channel_name)
-            
-            await self.accept()
-            print(f"✅ Usuário {user.username} conectado aos grupos: {self.group_names}")
-        else:
-            await self.close()
+        # Temporariamente, vamos aceitar QUALQUER conexão
+        await self.accept()
+        print("✅ Conexão WebSocket aceita com sucesso (Modo Teste)!")
 
     async def disconnect(self, close_code):
-        # Remove a conexão de todos os grupos em que foi adicionado
-        if hasattr(self, 'group_names'):
-            for group_name in self.group_names:
-                await self.channel_layer.group_discard(group_name, self.channel_name)
         print(f"❌ Conexão encerrada.")
-
-    async def send_notification(self, event):
-        await self.send(text_data=json.dumps({
-            'type': 'notification',
-            'title': event['data']['title'],
-            'message': event['data']['message'],
-            'url': event['data'].get('url', '#')
-        }))
