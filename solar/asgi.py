3d6e9fc30af00a0b74c8ae4217651875d793446d
Ellen <daviddjustt@gmail.com>
2026-06-09 16:15:43 -0300

    Novo campo para o projeto chamado pedido_vistoria criado

--- solar/asgi.py
+++ solar/asgi.py
@@ -1,22 +1,20 @@
 import os
 import configurations
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solar.config.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solar.config.production') # Use o seu config correto
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 configurations.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from solar.notifications.middleware import JWTAuthMiddleware
 from solar.notifications.consumers import NotificationConsumer
 
+# Removemos o AuthMiddlewareStack para testar se é ele que derruba a conexão
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        URLRouter([
-            # Nota: usamos <str:user_id> para evitar problemas iniciais com o tipo UUID
-            path("ws/notifications/<str:user_id>/", NotificationConsumer.as_asgi()),
-        ])
-    ),
+    "http": django_asgi_app,
+    "websocket": URLRouter([
+        path("ws/notifications/", NotificationConsumer.as_asgi()),
+    ]),
 })
