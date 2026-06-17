ee3606de867e26900ea2c446d325f6cce753bc5b
Ellen <daviddjustt@gmail.com>
2026-06-17 12:52:43 -0300

    Removendo sistema de emails antigo

--- solar/documents/signals.py
+++ solar/documents/signals.py
@@ -2,7 +2,6 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import ClientProject, AndamentoDoProjeto, ProjectDocument
-from solar.users.email import StatusProjetoChangedEmail, DocumentRejectedEmail
 import logging
 
 logger = logging.getLogger(__name__)
